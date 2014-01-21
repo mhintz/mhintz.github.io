33caa97881d271459fbf8451e9d1a07a6870cae4
Mark Hintz <markohintz@gmail.com>
2014-01-20 23:37:38 -0800

    add portfolio page

--- js/model/SiteContent.js
+++ js/model/SiteContent.js
@@ -3,23 +3,73 @@ define([], function() {
 		"nav": {
 			"twitter": "https://twitter.com/MarkHintz",
 			"github": "http://www.github.com/mhintz/",
-			"email": "markohintz@gmail.com"
+			"email": ["mark", "o", "hintz", "@", "gmail", ".com"].join("")
 		},
 		"root": {
 
 		},
 		"work": {
-			
+			"header": "The following is a list of the publicly viewable projects I have worked on. It does not include prototypes, back-end work, data analysis, or projects that were intended for clients' internal use. To learn more about this and other work I've done, feel free to drop me a line.",
+			"portfolioItems": [
+				{
+					"title": "50 Years Later: John F. Kennedy's Life, Loss and Legacy",
+					"url": "http://nvcdn.nbcnews.com/_util/jfk50/",
+					"descriptionTitle": "A tool for exploring interview video clips.",
+					"descriptionBody": "NBC's Tom Brokaw conducted a series of interviews discussing the life and legacy of John F. Kennedy with a variety of politicians, media members, scholars, dignitaries, and citizens who were involved with the Kennedy family or the events surrounding the assassination. Periscopic partnered with NBC Universal to design and develop an interface for the video clips.",
+					"responsibility": "I implemented most of the site, for the most part working from design comps that had been created by Periscopic designers. I collaborated with another developer on the animation and layout of the thumbnails. Two particular challenges were to make the site functional in Internet Explorer 8, and to build the touch interactions. Late changes to the design and the addition of last-minute features were also a difficulty.",
+					"photos": [
+						"img/projects/nbc/nbc_1.jpg",
+						"img/projects/nbc/nbc_2.jpg",
+						"img/projects/nbc/nbc_3.jpg"
+					]
+				},
+				{
+					"title": "Creating change from 2008 to 2012",
+					"url": "http://www.adobe.com/corporate-responsibility/cr-reports.html",
+					"descriptionTitle": "Creatively Visualizing Data on the Progress of Corporate Responsibility Campaigns at Adobe",
+					"descriptionBody": "",
+					"responsibility": "",
+					"photos": [
+						"img/projects/adobe/adobe_1.jpg",
+						"img/projects/adobe/adobe_2.jpg",
+						"img/projects/adobe/adobe_3.jpg"
+					]
+				},
+				{
+					"title": "The Wait We Carry",
+					"url": "http://thewaitwecarry.org/",
+					"descriptionTitle": "Using Data to Tell the Stories of U.S. Veterans Waiting for Medical Assistance from the Government",
+					"descriptionBody": "",
+					"responsibility": "",
+					"photos": [
+						"img/projects/iava/iava_1.jpg",
+						"img/projects/iava/iava_2.jpg",
+						"img/projects/iava/iava_3.jpg"
+					]
+				},
+				{
+					"title": "U.S. Gun Deaths",
+					"url": "http://guns.periscopic.com/",
+					"descriptionTitle": "A Visualization of the Loss of Life from Gun Killings in the U.S.",
+					"descriptionBody": "",
+					"responsibility": "",
+					"photos": [
+						"img/projects/guns/guns_1.jpg",
+						"img/projects/guns/guns_2.jpg",
+						"img/projects/guns/guns_3.jpg"
+					]
+				}
+			]
 		},
 		"explorations": {
 			"choices": [
 				{
 					"name": "unknown",
-					"preview": "img/previews/unknown.png"
+					"preview": "img/explorations/unknown.png"
 				},
 				{
 					"name": "epitrochoid",
-					"preview": "img/previews/epitrochoid.png"
+					"preview": "img/explorations/epitrochoid.png"
 				}
 			]
 		},
