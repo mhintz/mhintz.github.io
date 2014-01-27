c72051373408181eef9632b26d5c697b01ad1a38
Mark Hintz <markohintz@gmail.com>
2014-01-26 22:02:28 -0800

    update content

--- js/model/SiteContent.js
+++ js/model/SiteContent.js
@@ -28,7 +28,7 @@ define([], function() {
 					"url": "http://www.adobe.com/corporate-responsibility/cr-reports.html",
 					"descriptionTitle": "Creatively Visualizing Data on the Progress of Corporate Responsibility Campaigns at Adobe",
 					"descriptionBody": "A creative, data art-inspired look at corporate responsibility initiatives at Adobe. We wanted to do an abstract presentation that would reveal the data behind it as the user interacted with the tool. In particular we wanted animations that would feel surprising, but also natural.",
-					"responsibility": "I first built an early prototype to test different concepts. With this prototype we discovered the forms that we would end up using in the final piece. I then implemented the final design for the site, along with all interaction and graphics. My favorite part, but also the most difficult to implement, was the passive animation that occurs on the main page, as the shapes fade one by one into view along with a caption.",
+					"responsibility": "I first built an early prototype to test different concepts. With this prototype we discovered the forms that we would end up using in the final piece. I then implemented the final design for the site, along with all interaction and graphics. My favorite part, but also the most complicated to implement, was the passive animation that occurs on the main page, as the shapes fade one by one into view along with a caption.",
 					"photos": [
 						"img/projects/adobe/adobe_1.jpg",
 						"img/projects/adobe/adobe_2.jpg",
@@ -39,8 +39,8 @@ define([], function() {
 					"title": "The Wait We Carry",
 					"url": "http://thewaitwecarry.org/",
 					"descriptionTitle": "Using Data to Tell the Stories of U.S. Veterans Waiting for Medical Assistance from the Government",
-					"descriptionBody": "",
-					"responsibility": "",
+					"descriptionBody": "This was a project to visualize data collected from a survey of U.S. veterans of the wars in Iraq and Afghanistan that received support from the Veterans' Administration for medical conditions stemming from their service. These conditions included bodily aches and pains, stress injuries, psychological conditions, and physical injuries incurred in combat. Many veterans spent inordinate time waiting for the VA to process their requests for assistance.",
+					"responsibility": "I was the front-end developer for this project, which included a video intro and a large amount of user-supplied data. The data was unprocessed, so it was occasionally strange. The automatic video intro proved to be a challenge. This was the first project for which I used require.js to manage module and dependency loading, which expanded my understanding of object-oriented program architecture.",
 					"photos": [
 						"img/projects/iava/iava_1.jpg",
 						"img/projects/iava/iava_2.jpg",
@@ -51,8 +51,8 @@ define([], function() {
 					"title": "U.S. Gun Deaths",
 					"url": "http://guns.periscopic.com/",
 					"descriptionTitle": "A Visualization of the Loss of Life from Gun Killings in the U.S.",
-					"descriptionBody": "",
-					"responsibility": "",
+					"descriptionBody": "After the 2012 mass shooting at Sandy Hook elementary school, we at Periscopic decided to create a visualization using data on homicides collected by the FBI. We looked in particular at killings by gun, which are so alarmingly frequent in the United States, and sought to highlight the tragedy of the loss of the victims' years of life. Using data from the World Health Organization, we put together a very rough life expectancy generator to give an idea of the amount of lost life.",
+					"responsibility": "This was my first major programming project at Periscopic, and represented a major learning experience for me. I spent a lot of time collaborating with the designer to get the look and feel just right.",
 					"photos": [
 						"img/projects/guns/guns_1.jpg",
 						"img/projects/guns/guns_2.jpg",
