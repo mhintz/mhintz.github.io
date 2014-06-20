00f6d8651e79a8dd9e442b92e95173c34a9573c7
Mark Hintz <markohintz@gmail.com>
2014-06-20 07:50:42 -0700

    work on skybox

--- slides/osb_2014_net_art/js/example.js
+++ slides/osb_2014_net_art/js/example.js
@@ -1,17 +1,19 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
+var renderer;
 var APP = {};
+var SKY_SIZE = 1000;
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+	renderer = new THREE.WebGLRenderer();
+	renderer.sortObjects = false;
+	renderer.autoClear = false;
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setFaceCulling(THREE.CullFaceFront, THREE.FrontFaceDirectionCW);
+//	renderer.setFaceCulling(THREE.CullFaceFront, THREE.FrontFaceDirectionCW);
 
 	document.body.appendChild(renderer.domElement);
-
-	APP.skyBox = createCube(100, 100, 100, basicMaterial(randColor()));
 
 	APP.newCube = createCube(1, 1, 1, basicMaterial(randColor()));
 
@@ -52,3 +54,28 @@ function randColor() {
 		b = Math.round(Math.random() * 255);
 	return new THREE.Color("rgb("+r+","+g+","+b+")");
 }
+
+function setupSkyBox() {
+	var textureCube = THREE.ImageUtils.loadTextureCube(SKYBOX_URLS, new THREE.UVMapping()),
+		shader = THREE.ShaderUtils.lib.cube,
+		uniforms = THREE.UniformsUtils.clone(shader.uniforms);
+
+	uniforms.tCube.texture = textureCube;
+
+	var material = new THREE.MeshShaderMaterial({
+		fragmentShader: shader.fragmentShader,
+		vertexShader: shader.vertexShader,
+		uniforms: uniforms
+	}),
+	skyboxGeom = new THREE.CubeGeometry(SKY_SIZE, SKY_SIZE, SKY_SIZE);
+	APP.skyBox = new THREE.Mesh(skyboxGeom, material);
+	scene.addObject(APP.skyBox);
+}
+
+var SKYBOX_URL_ROOT = "lib/img/",
+	SKYBOX_URL_MIDS = ["posx", "negx", "posy", "negy", "posz", "negz"],
+	SKYBOX_URL_END = ".jpg",
+	SKYBOX_URLS = SKYBOX_URL_MIDS.map(function(name) {
+		return SKYBOX_URL_ROOT+name+SKYBOX_URL_END;
+	});
+
